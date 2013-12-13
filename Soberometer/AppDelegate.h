f250f9887d15a141b5a0ef4c29403472ac6d142b
Sujit Anand <sujit_anand@hotmail.com>
2013-12-12 17:34:57 -0800

    Initial Commit

---
+++ Soberometer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Soberometer
+//
+//  Created by Sujit Anand on 12/12/13.
+//  Copyright (c) 2013 Sujit Anand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
